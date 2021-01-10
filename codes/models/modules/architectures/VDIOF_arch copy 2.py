fb62f7f13139e427648c006274b28f9444372dce
Joey Ballentine <joeyjballentine@gmail.com>
2021-01-10 17:56:42 -0600

    wip files eh

--- codes/models/modules/architectures/VDIOF_arch copy 2.py
+++ codes/models/modules/architectures/VDIOF_arch copy 2.py
@@ -675,3 +675,99 @@ class VDOFNet(nn.Module):
 
         return flow_L1, flow_L2, flow_L3, out
         
+
+class VDOFNet_almost(nn.Module):
+    '''
+    Video Deinterlacing with Optical Flow Warping (VDOF)
+    '''
+    def __init__(self, in_nc=3, out_nc=3, nf=64, act_type='leakyrelu', channels=320, n_frames=3):
+        super(VDOFNet, self).__init__()
+
+        # Optical Flow Estimation Step
+        # Use motion estimation to restore center frame
+        self.OFR = OFRnet(channels, 6)
+
+        sr_in_nc=2*in_nc*((n_frames-1) +1)
+
+        body = []
+        body.append(nn.Conv2d(sr_in_nc, channels, 3, 1, 1, bias=False))
+        body.append(nn.LeakyReLU(0.1, inplace=True))
+        body.append(CasResB(8, channels))
+        body.append(nn.Conv2d(channels, 64 * 1, 1, 1, 0, bias=False))
+        body.append(nn.LeakyReLU(0.1, inplace=True))
+        body.append(nn.Conv2d(64, out_nc*2, 3, 1, 1, bias=True))
+        self.draft_cube_conv = nn.Sequential(*body)
+
+    def motion_estimation(self, x):
+        b, n_frames, c, h, w = x.size()
+        idx_center = (n_frames - 1) // 2
+
+        flow_L1 = []
+        flow_L2 = []
+        flow_L3 = []
+        input = []
+
+        for idx_frame in range(n_frames):
+            if idx_frame != idx_center:
+                input.append(torch.cat((x[:,idx_frame,:,:,:], x[:,idx_center,:,:,:]), 1))
+        optical_flow_L1, optical_flow_L2, optical_flow_L3 = self.OFR(torch.cat(input, 0))
+
+        optical_flow_L1 = optical_flow_L1.view(-1, b, 2, h//2, w//2)
+        optical_flow_L2 = optical_flow_L2.view(-1, b, 2, h, w)
+        optical_flow_L3 = optical_flow_L3.view(-1, b, 2, h, w)
+
+        # motion compensation
+        draft_cube = []
+        draft_cube.append(x[:, idx_center, :, :, :])
+
+        for idx_frame in range(n_frames):
+            if idx_frame == idx_center:
+                flow_L1.append([])
+                flow_L2.append([])
+                flow_L3.append([])
+            else: # if idx_frame != idx_center:
+                if idx_frame < idx_center:
+                    idx = idx_frame
+                if idx_frame > idx_center:
+                    idx = idx_frame - 1
+
+                flow_L1.append(optical_flow_L1[idx, :, :, :, :])
+                flow_L2.append(optical_flow_L2[idx, :, :, :, :])
+                flow_L3.append(optical_flow_L3[idx, :, :, :, :])
+
+                # Generate the draft_cube by subsampling the SR flow optical_flow_L3
+                # according to the scale
+                for i in range(1):
+                    for j in range(1):
+                        draft = optical_flow_warp(x[:, idx_frame, :, :, :],
+                                                  optical_flow_L3[idx, :, :, i::1, j::1] / 1)
+                        draft_cube.append(draft)
+        draft_cube = torch.cat(draft_cube, 1)
+        # print('draft_cube:', draft_cube.shape) #TODO
+
+        return flow_L1, flow_L2, flow_L3, draft_cube
+
+        
+    def forward(self, x):
+        # B, T, C, H, W
+        b, n_frames, c, h, w = x.size()
+
+        # Split tensor into fields
+        odd = x[:, :, :, 0::2, :]
+        even = x[:, :, :, 1::2, :]
+        # Concat fields on channel dimension
+        x = torch.cat((odd, even), 2) # B, T, C*2, H//2, W
+        # Concat 3 frames on channel dimension
+        x = torch.cat([x[:, t, :, :, :] for t in range(n_frames)], 1) # B, C*2*T, H//2, W
+        # Resize to full heigh 
+        x = F.interpolate(x, size=(h, w), mode='nearest') # B, C*2*T, H, W
+        # View back to 3 concatenated fields
+        x = x.view(b, n_frames, c*2, h, w) # B, T, C*2, H, W
+
+        # Optical Flow Motion Estimation
+        flow_L1, flow_L2, flow_L3, draft_cube = self.motion_estimation(x)
+
+        # Convert draft cube into 6 channel image (2 concatenated frames)
+        out = self.draft_cube_conv(draft_cube)
+
+        return flow_L1, flow_L2, flow_L3, out
